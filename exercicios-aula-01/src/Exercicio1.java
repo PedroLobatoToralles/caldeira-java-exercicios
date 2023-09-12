125f30f52de9f24e6f634a04a9ce486c1bc1a39e
Pedro Toralles <pedrotoralles@outlook.com>
2023-09-12 16:56:49 -0300

    Aula 2

---
+++ exercicios-aula-01/src/Exercicio1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Exercicio1 {
+    public void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //--------------- Exercício 1 ---------------//
+
+        System.out.println("Escreva seu nome:");
+        String nome = scanner.nextLine();
+        System.out.println("Bem-vindo, " + nome);
+
+    }
+}
